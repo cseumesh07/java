0e6ed0c2a28108b701b9bcb7872fdf79c1ea968e
umeshlaxmi07 <klau.tiwari1234@gmail.com>
2021-09-29 23:44:57 +0530

    bug fixes

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
  public static void main(String[] args) {
    Scanner scanner = new Scanner(System.in);
 
-   System.out.println("Enter yout age");
+   System.out.println("Enter your age");
    System.out.println("My age is :" + scanner.next());
    scanner.close();
  }
